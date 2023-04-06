c1e4b9eae8a20248507affa8a5dfcd7dfc52faa2
Ian Neidel <68819605+ianneidel@users.noreply.github.com>
2023-04-06 14:01:00 -0400

    updated to be color blind friendly

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#D3D3D3');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#000000');
   }
   updateSnakeCoordinates();
   updateHandContainer();
